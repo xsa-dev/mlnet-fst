3a9da78a13235c89cfb42c63c005dcd02495bfef
xsa <xsa24@icloud.com>
2019-08-24 04:05:02 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.ML;
+using SampleBinaryClassification.Model.DataModels;
+
+namespace consumeModelApp {
+    class Program {
+        static void Main (string[] args) {
+            ConsumeModel ();
+        }
+
+        public static void ConsumeModel () {
+                // Load the model
+                MLContext mlContext = new MLContext ();
+
+                ITransformer mlModel = mlContext.Model.Load ("MLModel.zip", out var modelInputSchema);
+                var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput> (mlModel);
+
+                // Use the code below to add input data
+                var input = new ModelInput ();
+                input.SentimentText = "That is rude.";
+
+                // Try model on sample data
+                // True is toxic, false is non-toxic
+                ModelOutput result = predEngine.Predict (input);
+
+            Console.WriteLine($"Text: {input.SentimentText} | Prediction: { (Convert.ToBoolean(result.Prediction) ? "Toxic" : "Non-toxic")} sentiment");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# WHATS IT IS
+
+[ML.NET Tutorial - Get started in 10 minutes](https://dotnet.microsoft.com/learn/ml-dotnet/get-started-tutorial/intro)
+
+##mlnet 
